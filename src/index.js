dad1016da1265808592270183a44fec64a8c09f8
裴清庄 <peiqingzhuang@life.local>
2022-06-19 12:18:56 +0800

    update react version 18.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'reduxjs-toolkit-persist/integration/react'
 import App from '~/App'
@@ -7,8 +7,8 @@ import store, { persistor } from '~/redux'
 import GlobalStyle from './components/GlobalStyles'
 import reportWebVitals from './reportWebVitals'
 
-// const root = ReactDOM.createRoot(document.getElementById('root'))
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
     <React.StrictMode>
         <GlobalStyle>
             <Provider store={store}>
@@ -19,8 +19,7 @@ ReactDOM.render(
                 </PersistGate>
             </Provider>
         </GlobalStyle>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
