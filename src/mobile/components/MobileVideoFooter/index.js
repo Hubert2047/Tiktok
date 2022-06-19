a35a209407a73e20c06a69e57621ba3dbbc715a9
裴清庄 <peiqingzhuang@life.local>
2022-06-19 13:01:09 +0800

    remove react-ticker

--- src/mobile/components/MobileVideoFooter/index.js
+++ src/mobile/components/MobileVideoFooter/index.js
@@ -1,5 +1,6 @@
 import classNames from 'classnames/bind'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import LinesEllipsis from 'react-lines-ellipsis'
 import { SpinerBoxIcon } from '~/components/Icons'
 import Image from '~/components/Image'
 import styles from './MobileVideoFooter.module.scss'
@@ -9,6 +10,8 @@ const clsx = classNames.bind(styles)
 function MobileVideoFooter({ className, post }) {
     const spinerAnimated = useRef()
     const spinerRef = useRef()
+    const [reFlow, setReflow] = useState(false)
+    const [showAllContent, setShowAllContent] = useState()
     const handleClick = () => {
         if (spinerAnimated.current) {
             spinerAnimated.current.play()
@@ -30,27 +33,53 @@ function MobileVideoFooter({ className, post }) {
             spinerAnimated.current = _spiner
         }
     }, [])
-
+    const handleReflow = function (rleState) {
+        if (rleState?.clamped) {
+            setShowAllContent(false)
+            setReflow(true)
+        }
+    }
+    const handleShowContent = function () {
+        setShowAllContent((prev) => !prev)
+    }
     return (
         <div className={clsx('wrapper', 'd-flex', className)} onClick={handleClick}>
             <div className={clsx('footer-text')}>
-                <h3 className={clsx('title')}>{`@${post.user.nickname}`}</h3>
-                <div className={clsx('content-container', 'd-flex')}>
-                    <div className={clsx('music-icon')}></div>
-                    <div className={clsx('content-box')}>
-                        <h4 className={clsx('content')}>{post.content}</h4>
-                    </div>
+                <h3 className={clsx('name')}>{`@${post.user.nickname}`}</h3>
+                <div className={clsx('content-box')}>
+                    {post?.content?.length > 0 && !showAllContent ? (
+                        <LinesEllipsis
+                            className={clsx('content')}
+                            text={post.content}
+                            maxLine={2}
+                            basedOn='words'
+                            onReflow={handleReflow}
+                            ellipsis={'...'}></LinesEllipsis>
+                    ) : (
+                        <p className={clsx('content')}>{post.content}</p>
+                    )}
+                    {reFlow && (
+                        <button onClick={handleShowContent} className={clsx('show-content')}>
+                            See more
+                        </button>
+                    )}
                 </div>
             </div>
-            <div className={clsx('spiner-container')}>
-                <div className={clsx('spiner-song')}></div>
-                <div className={clsx('spiner-music')}></div>
-                <SpinerBoxIcon className={clsx('spiner-box')} />
-                <div ref={spinerRef} className={clsx('avatar-box')}>
-                    <Image
-                        className={clsx('avatar')}
-                        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDWrIiYIAdOLTJ5Ql5YtMUvL3y3kK0Vh5JXQ&usqp=CAU'
-                    />
+            <div className={clsx('bottom', 'd-flex')}>
+                <div className={clsx('title', 'd-flex')}>
+                    <div className={clsx('music-icon')}></div>
+                    <p>fafefaf</p>
+                </div>
+                <div className={clsx('spiner-container')}>
+                    <div className={clsx('spiner-song')}></div>
+                    <div className={clsx('spiner-music')}></div>
+                    <SpinerBoxIcon className={clsx('spiner-box')} />
+                    <div ref={spinerRef} className={clsx('avatar-box')}>
+                        <Image
+                            className={clsx('avatar')}
+                            src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDWrIiYIAdOLTJ5Ql5YtMUvL3y3kK0Vh5JXQ&usqp=CAU'
+                        />
+                    </div>
                 </div>
             </div>
         </div>
