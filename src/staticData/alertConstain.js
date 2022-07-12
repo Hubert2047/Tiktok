bab1ce373724d91f08d52bd8efdd87d56a138fd8
裴清庄 <peiqingzhuang@life.local>
2022-07-12 21:13:57 +0800

    fix upload video size limited text

--- src/staticData/alertConstain.js
+++ src/staticData/alertConstain.js
@@ -1,4 +1,4 @@
 const TEXT_LIMITED = 'Text is limited. Maximum 150 characters'
-const FILE_SIZE_LIMITED = 'Text is limited. Maximum 150 characters'
+const FILE_SIZE_LIMITED = 'Size is limited. Maximum 10Mb '
 
 export { TEXT_LIMITED, FILE_SIZE_LIMITED }
